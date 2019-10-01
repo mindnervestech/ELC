997979d35cb9fe854e65460843ab769ee5c6f1ce
Aaditya Gupta <aadityagmnt@gmail.com>
2019-10-01 21:32:15 +0530

    Fixes

--- src/js/components/Login/Login.js
+++ src/js/components/Login/Login.js
@@ -445,12 +445,12 @@ class Login extends Component {
                                           </div>
                                           </div>
                                         </div>
-                                        <div className="forgotPass" style={{ textAlign: 'right'}} >
+                                        {/* <div className="forgotPass" style={{ textAlign: 'right'}} >
                                           <a className="forget-password-link link" style={{width: "100%",fontWeight:'800 !important'}} onClick={this.onOpenSecondModal}><FormattedMessage id="forget.password" defaultMessage="Forget Password" /></a>
 
                                           <span id="P1000_FP_error_placeholder" className="a-Form-error" data-template-id="33609641515469732_ET"></span>
 
-                                        </div>
+                                        </div> */}
                                       </div>
                                       {/* <div >
                                           <div className="t-Form-fieldContainer rel-col  apex-item-wrapper apex-item-wrapper--checkbox " id="P1000_REMEMBER_CONTAINER">
