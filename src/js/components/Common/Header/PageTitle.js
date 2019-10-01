7e9bca2f9d725a246193f464da77094a923ae32f
Aaditya Gupta <aadityagmnt@gmail.com>
2019-10-01 11:30:08 +0530

    Fixes

--- src/js/components/Common/Header/PageTitle.js
+++ src/js/components/Common/Header/PageTitle.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
-import { injectIntl, FormattedMessage } from 'react-intl';
-
+import { injectIntl, formatMessage,FormattedMessage } from 'react-intl';
+import { withRouter } from 'react-router-dom';
 
 class PageTitle extends Component {
     constructor(props) {
+        console.log(props)
         super(props);
         this.myIntl = props.intl
         this.state = {
@@ -13,16 +14,44 @@ class PageTitle extends Component {
         }
     }
 
+
+ 
     componentDidMount() {
-        this.setState({
-            title: this.getTital(this.props.pageTitle)
-        })
-    }
+      
+        let pathname = this.props.location.pathname.split('/');
+        if(pathname[pathname.length -1]=='')
+        {
+            setTimeout(() => {
+                this.setState({
+                    title:this.getTital('NA')
+                }) 
+            }, 100);
+        }
+        else{
+            setTimeout(() => {
+                this.setState({
+                    title: this.getTital(pathname[pathname.length - 1])
+                }) 
+            }, 100);
+        }
+       
+      }
 
     componentDidUpdate(prevProps) {
-        if ((this.props.pageTitle !== prevProps.pageTitle) || (this.props.globals.language !== prevProps.globals.language)) {
+        let pathname = this.props.location.pathname.split('/');
+        if(pathname[pathname.length - 1]=='')
+        { 
+         
+            setTimeout(() => {
+                this.setState({
+                    title:this.getTital('NA')
+                }) 
+            }, 100);
+        }
+        else if (this.props.location.pathname !== prevProps.location.pathname) {
+            const pathname=this.props.location.pathname.split('/');
             this.setState({
-                title: this.getTital(this.props.pageTitle)
+                title: this.getTital(pathname[pathname.length - 1])
             })
         }
     }
@@ -36,12 +65,10 @@ class PageTitle extends Component {
         }else if(country === 'ksa'){
             country = 'KSA'
         }
-        let mainTitle = title !== 'NA' ? this.myIntl.formatMessage({ id: `PageTitle.${title}` }) : this.myIntl.formatMessage({ id: `PageTitle.Home` });
-        // `PageTitle.${title}` in intl.messages ? intl.formatMessage({ id: `PageTitle.${title}` }) : intl.formatMessage({ id: `PageTitle.Home` })
-        let appendTitle = this.myIntl.formatMessage({ id: `PageTitle.nayomi.${country}` })
-        // intl.formatMessage({ id: `PageTitle.nayomi.${country}` });
-
-        return `${mainTitle} | ${appendTitle}`
+    
+        let mainTitle = title !== 'NA' ? intl.formatMessage({ id: `PageTitle.${title}`,defaultMessage: 'Do you think {subject} should be informed?' }) : intl.formatMessage({ id: `PageTitle.Home` });
+    
+        return mainTitle + " | " + intl.formatMessage({ id: `PageTitle.nayomi.${country}` })
     }
 
 
@@ -58,8 +85,9 @@ class PageTitle extends Component {
 const mapStateToProps = state => {
     return {
         globals: state.global,
+      
     }
 }
 
 
-export default connect(mapStateToProps)(injectIntl(PageTitle));
+export default  withRouter (connect(mapStateToProps)(injectIntl(PageTitle)));
