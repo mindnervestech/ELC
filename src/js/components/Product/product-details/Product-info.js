5333c0bc27f572596040207b963d0361e1864014
mindnervestech <mindnervestech@gmail.com>
2019-10-11 14:41:45 +0530

    Bug fixes

--- src/js/components/Product/product-details/Product-info.js
+++ src/js/components/Product/product-details/Product-info.js
@@ -294,7 +294,8 @@ class ProductInfo extends Component {
 			</span>
 			</Link>);
 		} else {
-			return (<span onClick={()=>this._handleClick()}  className="wishlist-span-1 mr-10-wishlist"><svg
+			return ( !this.state.is_in_wishlist_item ?
+			<span onClick={()=>this._handleClick()}  className="wishlist-span-1 mr-10-wishlist"><svg
 				xmlns="http://www.w3.org/2000/svg"
 				xmlnsXlink="http://www.w3.org/1999/xlink"
 				version="1.1"
@@ -318,7 +319,39 @@ class ProductInfo extends Component {
 				</g>{' '}
 			</svg>
 				{this.state.is_in_wishlist_item ? <span style={{ margingRight: "35px" }} disabled="disabled"><FormattedMessage id="PageTitle.add-wishlist" defaultMessage="Add to wishlist" /></span> : <span disabled="disabled" style={{ margingRight: "35px" }}><FormattedMessage id="PageTitle.add-wishlist" defaultMessage="Add to wishlist" /></span>}
-			</span>);
+			</span>:
+
+
+                <span  className="wishlist-span-1 mr-10-wishlist"><svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				version="1.1"
+				id="Capa_1"
+				x="0px"
+				y="0px"
+				viewBox="0 0 50 50"
+				style={{ enableBackground: 'new 0 0 50 50', marginRight: 10 }}
+				xmlSpace="preserve"
+				width="20px"
+				height="20px"
+				disabled="disabled"
+				className={"naylove-icon " + (this.state.is_in_wishlist_item ? 'active' : '')}
+
+			>
+				<g transform="matrix(0.94148 0 0 0.94148 1.46299 1.46299)">
+					<path
+						d="M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543  c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503  c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z"
+						className="naylove"
+					/>
+				</g>{' '}
+			</svg>
+				{this.state.is_in_wishlist_item ? <span style={{ margingRight: "35px" }} disabled="disabled"><FormattedMessage id="PageTitle.add-wishlist" defaultMessage="Add to wishlist" /></span> : <span disabled="disabled" style={{ margingRight: "35px" }}><FormattedMessage id="PageTitle.add-wishlist" defaultMessage="Add to wishlist" /></span>}
+			</span>
+			
+			
+			);
+
+			
 		}
 	}
 
