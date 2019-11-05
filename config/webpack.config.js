5cc01f417e6d3e061ce630a27c56baab29734fc9
mindnervestech <mindnervestech@gmail.com>
2019-11-05 15:40:02 +0530

    changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -34,6 +34,9 @@ const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false';
 // Check if TypeScript is setup
 const useTypeScript = fs.existsSync(paths.appTsConfig);
 
+let date = new Date();
+var newDate = date.getTime();
+
 // style files regexes
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
@@ -151,12 +154,12 @@ module.exports = function(webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[chunkhash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        ? `static/js/[name].[chunkhash:8].js?v=${newDate}`
+        : isEnvDevelopment && `static/js/bundle.js?v=${newDate}`,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[chunkhash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
+        ? `static/js/[name].[chunkhash:8].chunk.js?v=${newDate}`
+        : isEnvDevelopment && `static/js/[name].chunk.js?v=${newDate}`,
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
       publicPath: publicPath,
@@ -539,8 +542,8 @@ module.exports = function(webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: `static/css/[name].[contenthash:8].css?v=${newDate}`,
+          chunkFilename: `static/css/[name].[contenthash:8].chunk.css?v=${newDate}`,
         }),
       // Generate a manifest file which contains a mapping of all asset filenames
       // to their corresponding output file so that tools can pick it up without
